8e383dab03596032e9f18b7441d2e69b4bf3770b
omarabdalazeemibrahim <omar.abdalazeem.fcis.asu@gmail.com>
2022-04-03 20:43:16 +0200

    Task Upload

---
+++ API/Context.js
@@ -0,0 +1,57 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { getNewsAPI, getSourceAPI } from "./api";
+
+
+
+export const NewsContext = createContext();
+
+
+const Context = ({ children }) => {
+    const [news, setnews] = useState([]);
+    const [category, setcategory] = useState("general");
+    const [index, setIndex] = useState(1);
+    const [source, setSource] = useState();
+    const [darkTheme, setDarkTheme] = useState(true);
+    
+
+
+    const fetchNews = async (reset = category) => {
+        const { data } = await axios.get(getNewsAPI(reset))
+        setnews(data);
+        setIndex(1)
+
+    };
+
+    const fetchNewsFromSource = async () => {
+        try {
+            const { data } = await axios.get(getSourceAPI(source));
+            setnews(data);
+            setIndex(1)
+        } catch (error) {
+            console.log(error)
+            console.log("error")
+        }
+    };
+
+
+    useEffect(() => {
+        fetchNews()
+    }, [category])
+
+    useEffect(() => {
+        fetchNewsFromSource()
+    }, [source])
+    return (<NewsContext.Provider value={{
+        news,
+        darkTheme,
+        index,
+        setIndex,
+        fetchNews,
+        setcategory,
+        setSource,
+        setDarkTheme
+    }}>{children}
+    </NewsContext.Provider>)
+}
+export default Context;
---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import { View, StyleSheet, Text, StatusBar, useWindowDimensions } from 'react-native'
+import Tabs from './Components/Tabs';
+import Context, { NewsContext } from './API/Context';
+import Navigation from './Components/Navigation';
+
+
+function App() {
+  const { darkTheme } = useContext(NewsContext);
+  return (
+    <View style={{ ...styles.container, backgroundColor: darkTheme? "#282C35":"white" }}>
+      <Navigation />
+    </View>
+  )
+}
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+
+    }
+  }
+)
+
+export default () => {
+  return (
+    <Context>
+      <App />
+    </Context>
+  )
+}
